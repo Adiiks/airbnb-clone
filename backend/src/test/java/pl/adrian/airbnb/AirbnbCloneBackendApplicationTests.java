d5d8d7d400d472d88c16e4dd816023bfeab1c18d
Adiks <apacholak97@interia.pl>
2023-11-28 10:48:43 +0100

    creating initial backend project

---
+++ backend/src/test/java/pl/adrian/airbnb/AirbnbCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adrian.airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
